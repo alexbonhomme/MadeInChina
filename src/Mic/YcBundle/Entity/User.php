b6e9820591f73f4afce5975bed55a5b80e994f7f
Mickael Duruisseau <mickael.duruisseau@gmail.com>
2013-12-06 01:35:59 +0100

    YC-10 : Ajout champs ids reseaux sociaux

--- src/Mic/YcBundle/Entity/User.php
+++ src/Mic/YcBundle/Entity/User.php
@@ -21,7 +21,27 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
-
+	
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_fb", type="integer", nullable=true)
+     */
+    protected $id_fb;
+	
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_twitter", type="integer", nullable=true)
+     */
+    protected $id_twitter;
+	
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id_google", type="integer", nullable=true)
+     */
+    protected $id_google;
 
     /**
      * Get id
@@ -31,5 +51,65 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+	   
+	 /**
+     * Get id_fb
+     *
+     * @return integer 
+     */
+    public function getId_fb()
+    {
+        return $this->id_fb;
+    }
+		   
+	 /**
+     * Set id_fb
+     *
+     * @return integer 
+     */
+    public function setId_fb($id_fb)
+    {
+        $this->id_fb = $id_fb;
+    }
+	   
+	 /**
+     * Get id_twitter
+     *
+     * @return integer 
+     */
+    public function getId_twitter()
+    {
+        return $this->id_twitter;
+    }
+		   
+	 /**
+     * Set id_twitter
+     *
+     * @return integer 
+     */
+    public function setId_twitter($id_twitter)
+    {
+        $this->id_twitter = $id_twitter;
+    }
+	   
+	 /**
+     * Get id_google
+     *
+     * @return integer 
+     */
+    public function getId_google()
+    {
+        return $this->id_google;
+    }
+		   
+	 /**
+     * Set id_google
+     *
+     * @return integer 
+     */
+    public function setId_google($id_google)
+    {
+        $this->id_google = $id_google;
     }
 }
