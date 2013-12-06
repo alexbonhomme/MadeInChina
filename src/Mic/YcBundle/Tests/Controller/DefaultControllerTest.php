7f508ab6c17d921bccce0e531600a57c4600e034
Mickael Duruisseau <mickael.duruisseau@gmail.com>
2013-12-06 06:59:41 +0100

    Suppression AcmeDemoBundle

--- src/Mic/YcBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Mic/YcBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testResults()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('POST', '/results', array('search' => "I'm looking for a big white pillow to sleep", 'results' => array()));
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("I\'m looking for a big white pillow to sleep")')->count() > 0);
     }
 }
